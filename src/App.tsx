80e85ee973db7686c3fd9492a08d83576b2ccb89
farukulWD <farukgb1999@gmail.com>
2024-02-04 17:43:01 +0600

    create slice

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@
 import { Button } from '@/components/ui/button'
 function App() {
   return (
-    <>
-     <Button>Click me</Button>
-    </>
+    <div className='h-screen flex justify-center items-center'>
+     <div className='flex gap-6 items-center p-10 border-2 border-green-400 rounded-sm'>
+      <Button className='bg-green-400 text-white hover:bg-green-500'>Increment</Button>
+      <h1>0</h1>
+      <Button className='bg-red-400 text-white hover:bg-red-500'>Decrement</Button>
+     </div>
+    </div>
   )
 }
 
